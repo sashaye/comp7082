9a80884cf4b9f2fe864bc7febd9761c0e5929675
ImPhinite <untildeathdousapar@gmail.com>
2019-09-30 20:52:00 -0700

    Added gallery

--- app/src/main/java/com/example/photogallery/PhotosFragment.java
+++ app/src/main/java/com/example/photogallery/PhotosFragment.java
@@ -8,12 +8,68 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class PhotosFragment extends Fragment {
+
+    private final String image_titles[] = {
+            "Img1",
+            "Img2",
+            "Img3",
+            "Img4",
+            "Img11",
+            "Img21",
+            "Img31",
+            "Img41",
+            "Img12",
+            "Img22",
+            "Img32",
+            "Img42",
+    };
+
+    private final Integer image_ids[] = {
+            R.drawable.img1,
+            R.drawable.img2,
+            R.drawable.img3,
+            R.drawable.img4,
+            R.drawable.img11,
+            R.drawable.img21,
+            R.drawable.img31,
+            R.drawable.img41,
+            R.drawable.img12,
+            R.drawable.img22,
+            R.drawable.img32,
+            R.drawable.img42,
+    };
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_photos, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_photos, container, false);
+
+        RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.photo_gallery);
+        recyclerView.setHasFixedSize(true);
+
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getActivity().getApplicationContext(), 2);
+        recyclerView.setLayoutManager(layoutManager);
+        ArrayList<CreateList> createLists = prepareData();
+        ImageAdapter adapter = new ImageAdapter(getActivity().getApplicationContext(), createLists);
+        recyclerView.setAdapter(adapter);
+
+        return rootView;
+    }
+
+    private ArrayList<CreateList> prepareData(){
+        ArrayList<CreateList> theimage = new ArrayList<>();
+        for(int i = 0; i< image_titles.length; i++){
+            CreateList createList = new CreateList();
+            createList.setImage_title(image_titles[i]);
+            createList.setImage_ID(image_ids[i]);
+            theimage.add(createList);
+        }
+        return theimage;
     }
 }
